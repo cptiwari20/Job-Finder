20d279931e252badefb2edba5c1241a7161cd6d8
Chandra Prakash Tiwari <cptiwari20@gmail.com>
2019-03-15 17:41:48 +0530

    added styled welcome screen

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react'
 import {Text, View} from 'react-native'
 import Slides from '../src/Slides';
 
+const SlidesData = [
+  { text: 'Welcome to the Job Finder App', color: '#5942f4'},
+  { text: 'Login using your facebook account', color: '#e241f4'},
+  { text: 'Find awesome job near you.', color: '#910b2d'}
+]
  class WelcomeScreen extends Component {
   render() {
     return (
-      <Slides />
+      <Slides slideData={SlidesData} />
     )
   }
 }
--- src/Slides.js
+++ src/Slides.js
@@ -1,13 +1,46 @@
 import React, { Component } from 'react';
-import {View, Text, ScrollView} from 'react-native'
+import {View, Text, ScrollView, Dimensions} from 'react-native'
 
 class Slides extends Component {
+  renderData = () => (
+    this.props.slideData.map((slide) => {
+      return (
+      <View 
+        style={[styles.slide, { backgroundColor: slide.color}]} 
+        key={slide.text}
+        >
+        <Text style={styles.text}>{slide.text}</Text>
+      </View>
+    )
+    }
+  ))
+
   render() {
     return (
-      <ScrollView>
-        <Text>Welcome to the Job Finder</Text>
+      <ScrollView 
+        style={{flex: 1}}
+        horizontal
+        pagingEnabled
+      >
+        {this.renderData()}
       </ScrollView>
     )
+  }
+}
+
+const WIDTH = Dimensions.get('window').width;
+
+const styles = {
+  slide: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: WIDTH
+  },
+  text: {
+    fontSize: 30,
+    padding: 20,
+    color: 'white'
   }
 }
 
