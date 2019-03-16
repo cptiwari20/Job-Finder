9ca08d8a698d7b4c93f5692f49f52fe172fd907b
Chandra Prakash Tiwari <cptiwari20@gmail.com>
2019-03-16 17:04:58 +0530

    connected action to auth component

--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -1,21 +1,18 @@
 import React, { Component } from 'react'
 import {Text, View} from 'react-native'
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
  class AuthScreen extends Component {
+   componentDidMount(){
+     return this.props.actions.loginToFacebook()
+   }
   render() {
     return (
       <View>
-        <Text>Auth Screen</Text>
-        <Text>Auth Screen</Text>
-        <Text>Auth Screen</Text>
-        <Text>Auth Screen</Text>
-        <Text>Auth Screen</Text>
-        <Text>Auth Screen</Text>
-        <Text>Auth Screen</Text>
-        <Text>Auth Screen</Text>
-
+        <Text>AuthScreen</Text>
       </View>
     )
   }
 }
-export default AuthScreen;
+export default connect(null, actions)(AuthScreen);
