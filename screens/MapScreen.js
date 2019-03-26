fea764bafb285c5386adab47d7b58640fbc75c6d
Chandra Prakash Tiwari <cptiwari20@gmail.com>
2019-03-26 20:17:07 +0530

    added map view with marker

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,20 +1,57 @@
 import React, { Component } from 'react'
-import {Text, View} from 'react-native'
+import {Text, View} from 'react-native';
+import { MapView, AppLoading} from 'expo';
 
  class MapScreen extends Component {
-  render() {
-    return (
-      <View>
-        <Text>Map Screen</Text>
-        <Text>Map Screen</Text>
-        <Text>Map Screen</Text>
-        <Text>Map Screen</Text>
-        <Text>Map Screen</Text>
-        <Text>Map Screen</Text>
-        <Text>Map Screen</Text>
-        <Text>Map Screen</Text>
+  state = {
+    region: {
+      latitude: 40.76727216,
+      longitude: -73.99392888,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    },
+    showMap: false,
+    markers: []
+  }
+  
+  componentDidMount(){
+    this.fetchData()
+    this.setState({ showMap: true})
+  }
 
-      </View>
+  onRegionChangeComplete = (region) => {
+    this.setState({ region })
+  }
+
+  async fetchData(){
+    const res = await fetch('https://feeds.citibikenyc.com/stations/stations.json');
+    const data = await res.json();
+    this.setState({ markers: data.stationBeanList });
+  }
+
+  render() {
+    if(!this.state.showMap){
+      return <AppLoading />
+    }
+    return (
+      <MapView 
+        showsUserLocation 
+        showsCompass={true}
+        style={{ flex: 1 }} 
+        region={this.state.region} 
+        onRegionChangeComplete={this.onRegionChangeComplete}
+        >
+        {this.state.markers.map((marker, index) => (
+          <MapView.Marker 
+          key={index}
+          coordinate={
+            {latitude: marker.latitude,
+            longitude: marker.longitude,}
+          }
+          title={marker.stationName}
+        />
+        ))}
+        </MapView>
     )
   }
 }
